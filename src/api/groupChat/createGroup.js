9a137e1912f00aca4657123f8c13e660ff239f9d
lzg <lizg@easemob.com>
2021-11-16 16:49:14 +0800

    feat: add success message

--- src/api/groupChat/createGroup.js
+++ src/api/groupChat/createGroup.js
@@ -1,24 +1,28 @@
 
 import WebIM from '../../utils/WebIM'
+import getGroups from './getGroups'
+import { message } from '../../components/common/alert'
+import i18next from "i18next";
 const createGroup = (groupInfo, member, onClose) => {
     const { groupNameValue, groupDescriptionValue, groupPublicChecked, groupInviteChecker } = groupInfo
     let options = {
         data: {
-            groupname: groupNameValue,          // 群组名
-            desc: groupDescriptionValue,       // 群组描述
-            members: member,     // 用户名组成的数组
-            public: groupPublicChecked,                    // pub等于true时，创建为公开群
-            approval: true,                  // approval为true，加群需审批，为false时加群无需审批
-            allowinvites: groupInviteChecker,      // true：允许群成员邀请人加入此群，false：只有群主才可以往群里加人 注意公开群（public：true),则不允许群成员邀请别人加入此群
-            inviteNeedConfirm: false         // 邀请加群，被邀请人是否需要确认。true 为需要被邀请者同意才会进群
+            groupname: groupNameValue,          
+            desc: groupDescriptionValue,      
+            members: member,     
+            public: groupPublicChecked,                  
+            approval: true,                 
+            allowinvites: groupInviteChecker,   
+            inviteNeedConfirm: false 
         },
-        success(res) {
-            onClose();
-        },
-        error(err) { },
+        
+          
     };
     WebIM.conn.createGroupNew(options).then((res) => {
-        console.log(res)
+        console.log('createGroupNew>>>',res)
+        message.success(`${i18next.t('created success')}`)
+        getGroups();
+        onClose();
     })
 }
 
--- src/api/groupChat/getGroups.js
+++ src/api/groupChat/getGroups.js
@@ -4,6 +4,7 @@ import store from '../../redux/store'
 import { groupListAciton } from '../../redux/actions'
 const getGroups = () => {
     WebIM.conn.getGroup().then((res) => {
+        console.log('getGroups>>>',res);
         store.dispatch(groupListAciton(res.data))
     })
 }
